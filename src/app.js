b86133aa851d0e036f72cb3cd0761643307ddc24
Graeme Henderson <graeme.189@gmail.com>
2019-08-06 14:56:01 +0100

    Added population calculation

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue ({
+    el: "#app",
+    data: {
+      countries: []
+    },
+    mounted() {
+      this.fetchCountries()
+    },
+    computed: {
+      totalWorldPopulation: function () {
+        return this.countries.reduce((sum, country) => {
+          return sum + country.population
+        }, 0);
+      },
+    },
+    methods: {
+      fetchCountries: function () {
+        const request = fetch("https://restcountries.eu/rest/v2/all")
+        .then(response => response.json())
+        .then(fetchedCountries => this.countries = fetchedCountries)
+      }
+      // fetchName: function() {
+      //   const request = fetch("https://restcountries.eu/rest/v2/name/Algeria")
+      //   .then(response => response.json())
+      //   .then(data => this.countryNameLink = data[0].name)
+      // },
+      // fetchPopulation: function() {
+      //   const request = fetch("https://restcountries.eu/rest/v2/name/Algeria")
+      //   .then(response => response.json())
+      //   .then(data => this.countryPopulation = data[0].population)
+      // }
+    }
+  })
+});
